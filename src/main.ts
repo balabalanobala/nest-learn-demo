6ffbc5498f99e3649988aa7d2bb5a07a7b1d2460
xiaona.li <xiaona.li@xctec.cn>
2023-01-12 10:40:40 +0800

    feat(conf): add hmr

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  // module.hot类型错误：需要安装 @types/webpack-env
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
